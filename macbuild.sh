18926686cc545179943ccf776deac2ce87c948d1
Simon Williams <hair.machine@gmail.com>
2016-12-17 15:13:20 +0000

    Commit message

--- macbuild.sh
+++ macbuild.sh
@@ -2,14 +2,14 @@ git add .
 git commit -m "$2"
 git push https://hairmachine:$1@github.com/HairMachine/megarogue master
 
-#ssh macair@139.59.173.86 << EOF
-#  cd megarogue
-#  make
-#  git add out.bin
-#  git commit -m "Automated build commit"
-#  git push https://hairmachine:$1@github.com/HairMachine/megarogue master
-#  exit
-#EOF
+ssh macair@139.59.173.86 << EOF
+  cd megarogue
+  make
+  git add out.bin
+  git commit -m "Automated build commit"
+  git push https://hairmachine:$1@github.com/HairMachine/megarogue master
+  exit
+EOF
 
-#git pull origin master
-#/./Applications/Kega\ Fusion.app/Contents/MacOS/Kega\ Fusion out.bin
+git pull origin master
+/./Applications/Kega\ Fusion.app/Contents/MacOS/Kega\ Fusion out.bin
