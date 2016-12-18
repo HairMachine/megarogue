9759d9784b74fcdfe10cf3afb3b70127913ba960
Simon Williams <simon.williams@thegreatweb.co>
2016-12-18 14:46:50 +0000

    Adding level gen; commit to test remote build (error locally)

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <genesis.h>
+#include <time.h>
+#include <stdlib.h>
 
 enum direction { DIR_NORTH, DIR_EAST, DIR_SOUTH, DIR_WEST };
 
 enum tile { TIL_NULL, TIL_FLOOR, TIL_WALL, TIL_PLAYER, TIL_GOBLIN };
+
+const u32 tile_1[8]=
+{
+    0x00111100,
+    0x01144110,
+    0x11244211,
+    0x11244211,
+    0x11222211,
+    0x11222211,
+    0x01122110,
+    0x00111100
+};
 
 struct Thing {
 	int xpos;
@@ -20,14 +34,13 @@ struct Thing blocker;
 
 void tile_draw(enum tile tilenum, int x, int y) {
 	switch (tilenum) {
-		case TIL_NULL: break;
-		case TIL_FLOOR: VDP_drawText(" ", x, y); break;
-		case TIL_WALL: VDP_drawText("#", x, y); break;
-		case TIL_GOBLIN: VDP_drawText("G", x, y); break;
-		case TIL_PLAYER: VDP_drawText("0", x, y); break;
+		case TIL_NULL:
+		case TIL_FLOOR: VDP_setTileMapXY(APLAN, TILE_ATTR_FULL(PAL3, 0, 0, 0, 1), x, y); break;
+		case TIL_WALL: VDP_setTileMapXY(APLAN, 1, x, y); break;
+		case TIL_GOBLIN: VDP_setTileMapXY(APLAN, TILE_ATTR_FULL(PAL2, 0, 1, 0, 1), x, y); break;
+		case TIL_PLAYER: VDP_setTileMapXY(APLAN, TILE_ATTR_FULL(PAL1, 0, 1, 0, 1), x, y); break;
 	}
 }
-
 
 struct Thing thing_make(enum tile t, int x, int y) {
 	struct Thing thing;
@@ -129,8 +142,29 @@ void thing_move_toward(struct Thing* t, int xpos, int ypos) {
 	}
 }
 
-void myJoyHandler(u16 joy, u16 changed, u16 state)
-{
+void level_generate_room(int minx, int miny, int maxx, int maxy) {
+    int sizex = rand() % maxx - minx;
+    int sizey = rand() % maxy - miny;
+    int offsx = rand() % maxx - minx - sizex;
+    int offsy = rand() % maxy - miny - sizey;
+    int x;
+    int y;
+    for (x = minx + offsx; x < sizex; ++x) {
+        for (y = miny + offsy; y < sizey; ++y) {
+            maparray[x + (y * 40)] = TIL_FLOOR;
+        }   
+    }
+}
+
+void level_generate() {
+    int i = 0;
+    for (i = 0; i < 1600; ++i) {
+        maparray[i] = TIL_WALL;
+    }
+    level_generate_room(0, 0, 12, 12);
+}
+
+void joypad_handle(u16 joy, u16 changed, u16 state) {
     int turn = 0;
     if (joy == JOY_1) {
         if (state & BUTTON_UP) {
@@ -163,10 +197,13 @@ void myJoyHandler(u16 joy, u16 changed, u16 state)
     }
 }
 
-int main( )
-{
+int main() {
+    srand(time(NULL));
     JOY_init();
-    JOY_setEventHandler(&myJoyHandler);
+    JOY_setEventHandler(&joypad_handle);
+
+    //we load our unique tile data at position 1 on VRAM
+    VDP_loadTileData( (const u32 *)tile_1, 1, 1, 0); 
 
     empty = thing_make(TIL_NULL, 0, 0);
 
@@ -181,18 +218,7 @@ int main( )
 
     blocker = thing_make(TIL_WALL, 0, 0);
 
-    int i = 0;
-    for (i = 0; i < 1600; ++i) {
-    	maparray[i] = TIL_FLOOR;
-    }
-
-    maparray[325] = TIL_WALL;
-    maparray[123] = TIL_WALL;
-    maparray[23] = TIL_WALL;
-    maparray[479] = TIL_WALL;
-    maparray[999] = TIL_WALL;
-    maparray[157] = TIL_WALL;
-    maparray[75] = TIL_WALL;
+    level_generate();
 
     int x = 0, y = 0, j = 0;
     for (j = 0; j < 1600; ++j) {
@@ -212,6 +238,7 @@ int main( )
 
  	player = thing_make(TIL_PLAYER, 0, 0);
     tile_draw(player.til, player.xpos, player.ypos);
+    
 
     while(1) {
         VDP_waitVSync();
