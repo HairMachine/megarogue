2b20bef04417c0977e37b1d3ad850f28b77527fb
Simon Williams <hair.machine@gmail.com>
2016-12-28 17:54:25 +0000

    Adding keys and doors

--- main.c
+++ main.c
@@ -973,7 +973,7 @@ void level_generate() {
 	}
 
 	// Fourth part: remove silly doors
-	int c;
+	/*int c;
 	for (i = 0; i < maparraysize; ++i) {
 		if (maparray[i] == TIL_DOOR_EW || maparray[i] == TIL_DOOR_NS) {
 			// doors with more than 2 floors or corridors adjacent get turned into corridors
@@ -988,7 +988,7 @@ void level_generate() {
 			if (c > 2)
 				maparray[i] = TIL_CORRIDOR;
 		}
-	}
+	}*/
 
 	// Put stuff
 	struct vect2d ppos = position_find_valid();
