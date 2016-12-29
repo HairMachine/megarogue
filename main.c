7bda07c23fe4d3d72eb878a3a25740c2ad4e0dec
Simon Williams <hair.machine@gmail.com>
2016-12-29 17:51:56 +0000

    Tweaking probabilities

--- main.c
+++ main.c
@@ -594,7 +594,7 @@ void things_generate() {
 	}
 	// second loop: items.
 	for (i = 15; i < max_i; ++i) {
-		roll = gsrand(0, 10);
+		roll = gsrand(0, 9);
 		if (roll >= 0  && roll <= 1)
 			things[i] = thing_put(TIL_POTION);
 		else if (roll >= 2 && roll <= 3)
@@ -605,9 +605,9 @@ void things_generate() {
 			things[i] = thing_put(TIL_AMMO);
 		else if (roll == 7)
 			things[i] = thing_put(TIL_PIT);
-		else if (roll >= 8 && roll <= 9)	
+		else if (roll == 8)	
 			things[i] = thing_put(TIL_RAGE);
-		else if (roll == 10)
+		else if (roll == 9)
 			things[i] = thing_put(TIL_TELE);
 	}
 	// finally the stairs or macguffin on last level
