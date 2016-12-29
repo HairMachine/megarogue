b6c1ca9784bb22b05d13258291dbd3272c828160
Simon Williams <hair.machine@gmail.com>
2016-12-29 15:41:21 +0000

    Maximum health added

--- main.c
+++ main.c
@@ -467,7 +467,7 @@ void things_generate() {
 	if (max_m > 15) max_m = 15;
 	// int max_i = depth + 17;
 	// if (max_i > 27) max_i = 27;
-	int max_i = (10 + depth) >> 1;
+	int max_i = 10 + (depth >> 1);
 	// first loop: monsters.
 	for (i = 0; i < max_m; ++i) {
 		things[i] = thing_put(TIL_GOBLIN);
